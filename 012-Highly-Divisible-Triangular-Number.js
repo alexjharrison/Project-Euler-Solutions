1bb722633485b098c6d32fbe675c3ee77b20a266
Alex Harrison <alexjharrison@gmail.com>
2018-07-19 14:22:44 -0400

    solved 12

---
+++ 012-Highly-Divisible-Triangular-Number.js
@@ -0,0 +1,18 @@
+var divisors = 0;
+var count = 1;
+var lastNum = 1;
+
+while (divisors <= 500) {
+    divisors = 0;
+    count++;
+    lastNum += count;
+    for (var i = 1; i <= Math.floor(Math.sqrt(lastNum)); i++) {
+        if (i * i === lastNum) divisors++;
+        else if (lastNum % i === 0) divisors += 2;
+    }
+}
+
+console.log(lastNum);
+
+
+
