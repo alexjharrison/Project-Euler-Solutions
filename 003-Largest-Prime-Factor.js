49932cd84eaf2ba229bb0caaaa44024810a00e90
Alex Harrison <alexjharrison@gmail.com>
2018-07-19 09:21:38 -0400

    made grid into 2d array on p11

--- 003-Largest-Prime-Factor.js
+++ 003-Largest-Prime-Factor.js
@@ -5,7 +5,7 @@ var input = 600851475143;
 var isPrime = (num) => {
     if (num < 8 && num !== 4 && num != 6) return true;
     else if (num < 10) return false;
-    for (var i = 2; i < Math.floor(Math.sqrt(num)); i++) {
+    for (var i = 2; i <= Math.floor(Math.sqrt(num)); i++) {
         if (num % i === 0) {
             console.log(i);
             return false;
